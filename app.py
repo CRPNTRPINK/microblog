ea4b3584ec89e01ccb5e5d13610a56bc46a4698d
CRPNTRPINK <ultrololl@gmail.com>
2020-05-07 09:25:56 +0300

    'new'

--- app.py
+++
@@ -1,7 +0,0 @@
-from flask import Flask, render_template
-from flask_sqlalchemy import SQLAlchemy
-app = Flask(__name__)
-app.config.from_object('config')
-db = SQLAlchemy(app)
-
-import views, models
